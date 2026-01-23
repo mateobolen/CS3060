2b88ad4d632955fded569bf1bc4d0463a2aea4e2
mateo <mbolen@uvm.edu>
2026-01-23 13:40:31 -0500

    update simulat.py

--- simulate.py
+++ simulate.py
@@ -2,7 +2,6 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 for step in range(1000):
 	p.stepSimulation()
 	time.sleep(0.016)
