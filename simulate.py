700ae312fef570a2d3d24a9dcdcb8ffdc0cb0b83
mateo <mbolen@uvm.edu>
2026-01-25 13:39:44 -0500

    assignment 2 commit

--- simulate.py
+++ simulate.py
@@ -2,9 +2,22 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.loadSDF("box.sdf")
+
+"""
 for step in range(1000):
 	p.stepSimulation()
 	time.sleep(0.016)
 	print(step)
+"""
+
+step = 0
+while True:
+	p.stepSimulation()
+	time.sleep(0.016)
+	step += 1
+	print(step)
+
 p.disconnect()
