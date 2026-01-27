281c4154ab9f52ed65fe4db058340a6343bdb67e
mateo <mbolen@uvm.edu>
2026-01-27 14:37:18 -0500

    assignemnt 2 final commit

--- generate.py
+++ generate.py
@@ -1,11 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
 
-pyrosim.End() 
+for i in range(5):
+	for j in range(5):
+		for k in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[i,j,0.5 + k] , size=[length,width,height])
+			length *= 0.9
+			width *= 0.9
+			height *= 0.9
+		length = 1
+		width = 1
+		height = 1
+
+pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -4,11 +4,11 @@ import time
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 """
 for step in range(1000):
@@ -20,8 +20,8 @@ for step in range(1000):
 step = 0
 while True:
 	p.stepSimulation()
-	time.sleep(0.016)
+	time.sleep(0.001)
 	step += 1
-	print(step)
+	#print(step)
 
 p.disconnect()
